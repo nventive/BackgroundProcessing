76919f465fa7948a6303740571e27c470aec0839
Julien Blin <julien.blin@nventive.com>
2019-07-17 15:01:49 -0400

    Added Core.ServiceCollectionExtensions.

---
+++ BackgroundProcessing.Core/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using BackgroundProcessing.Core;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// <see cref="IServiceCollection"/> extension methods.
+    /// </summary>
+    public static class ServiceCollectionExtensions
+    {
+        /// <summary>
+        /// Adds the services necessary to process background tasks locally. <see cref="IBackgroundProcessor"/>.
+        /// </summary>
+        /// <param name="services">The <see cref="IServiceCollection"/>.</param>
+        /// <returns>The updated <see cref="IServiceCollection"/>.</returns>
+        public static IServiceCollection AddLocalBackgroundProcessor(this IServiceCollection services)
+            => services.AddScoped<IBackgroundProcessor, ServiceProviderBackgroundProcessor>();
+    }
+}
