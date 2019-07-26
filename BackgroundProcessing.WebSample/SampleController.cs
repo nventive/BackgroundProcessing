5b84d7148ce0b8938da2696985df95ea0280fe7c
Julien Blin <julien.blin@gmail.com>
2019-07-26 08:02:28 -0400

    Fixed web sample.

--- BackgroundProcessing.WebSample/SampleController.cs
+++ BackgroundProcessing.WebSample/SampleController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using BackgroundProcessing.Core;
 using BackgroundProcessing.Core.Events;
 using Microsoft.AspNetCore.Http;
@@ -8,18 +7,25 @@ using Microsoft.AspNetCore.Mvc;
 namespace BackgroundProcessing.WebSample
 {
     // See http://restalk-patterns.org/long-running-operation-polling.html
+    [ApiController]
     public class SampleController : ControllerBase
     {
         [HttpPost("api/v1/commands")]
+        [ProducesResponseType(StatusCodes.Status202Accepted)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> DispatchCommand(
             [FromServices] IBackgroundDispatcher dispatcher)
         {
             var command = new SampleCommand();
             await dispatcher.DispatchAsync(command);
-            return AcceptedAtAction(nameof(GetCommandStatus), new { id = command.Id });
+            return AcceptedAtRoute(nameof(GetCommandStatus), new { id = command.Id });
         }
 
-        [HttpGet("api/v1/commands/{id}")]
+        [HttpGet("api/v1/commands/{id}", Name = nameof(GetCommandStatus))]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status303SeeOther)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> GetCommandStatus(
             [FromRoute] string id,
             [FromServices] IBackgroundCommandEventRepository repository)
@@ -36,16 +42,19 @@ namespace BackgroundProcessing.WebSample
                 case BackgroundCommandEventStatus.Processing:
                     // Indicate the desired polling frequency, in seconds.
                     Response.Headers.Add("Retry-After", "10");
-                    return Ok();
+                    return NoContent();
                 case BackgroundCommandEventStatus.Processed:
                     // Once executed, redirect to the output.
-                    return this.SeeOther(Url.Action(nameof(GetCommandOutput), new { id }));
+                    return this.SeeOther(Url.RouteUrl(nameof(GetCommandOutput), new { id }));
                 default:
                     return StatusCode(StatusCodes.Status500InternalServerError);
             }
         }
 
-        [HttpGet("api/v1/commands/{id}/output")]
+        [HttpGet("api/v1/commands/{id}/output", Name = nameof(GetCommandOutput))]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesDefaultResponseType]
         public async Task<IActionResult> GetCommandOutput([FromRoute] string id)
         {
             // This part is up to you / your business logic.
